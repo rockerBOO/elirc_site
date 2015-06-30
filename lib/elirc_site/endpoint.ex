002eeef693e27667c04e213f4161dac8e3f4361a
rockerBOO <rockerboo@gmail.com>
2015-06-30 18:29:43 -0400

    Adding svgs to the endpoint static

--- lib/elirc_site/endpoint.ex
+++ lib/elirc_site/endpoint.ex
@@ -7,7 +7,7 @@ defmodule ElircSite.Endpoint do
   # when deploying your static files in production.
   plug Plug.Static,
     at: "/", from: :elirc_site, gzip: false,
-    only: ~w(css images js favicon.ico robots.txt)
+    only: ~w(css images js svg favicon.ico robots.txt)
 
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
